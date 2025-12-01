20edc68d168f8548595c13e7b32f5772b0217a49
oezy-coder <a8609556@gmail.com>
2025-12-01 12:33:47 +0900

    feat: UserRepository 이메일 중복 확인 메서드 추가 및 주석 작성

--- src/main/java/com/example/schedulemanagementdevelop/user/repository/UserRepository.java
+++ src/main/java/com/example/schedulemanagementdevelop/user/repository/UserRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 이메일로 유저 조회
     Optional<User> findByEmail(String email);
+
+    // 이메일 중복 여부 확인
+    boolean existsByEmail(String email);
 }
