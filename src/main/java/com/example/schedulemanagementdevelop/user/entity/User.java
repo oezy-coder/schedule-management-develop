858f9788946424b89648344317ba60b80e7198bb
oezy-coder <a8609556@gmail.com>
2025-12-01 09:49:38 +0900

    refactor: User 엔티티의 수정 기능 메서드 분리 - modifyUserName(), modifyPassword() 메서드를 User 엔티티에 추가

--- src/main/java/com/example/schedulemanagementdevelop/user/entity/User.java
+++ src/main/java/com/example/schedulemanagementdevelop/user/entity/User.java
@@ -27,8 +27,11 @@ public class User extends BaseEntity {
         this.password = password;
     }
 
-    public void update(String userName, String password) {
-        this.userName = userName;
-        this.password = password;
+    public void modifyUserName(String newUserName) {
+        this.userName = newUserName;
+    }
+
+    public void modifyPassword(String newPassword) {
+        this.password = newPassword;
     }
 }
